f7836dc763e221e2945a4d16659005870c83f87e
Teodor Garzdin <tsg@miracle.dk>
2017-05-07 18:11:54 +0200

    Initial commit. Import AdminLTE less files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './less/less/AdminLTE.less';
+import './less/less/skins/skin-blue.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+      </div>
+    );
+  }
+}
+
+export default App;
