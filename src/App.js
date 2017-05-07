810b9a6be299397c4b0195a93211a1b99dcc3bee
Teodor Garzdin <tsg@miracle.dk>
2017-05-07 18:47:33 +0200

    Port html components to react components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import './less/less/AdminLTE.less';
-import './less/less/skins/skin-blue.less';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Content from './Content';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
+      <div>
+        <Header />
+        <Sidebar />
+        <Content />
+        <Footer />
       </div>
     );
   }
